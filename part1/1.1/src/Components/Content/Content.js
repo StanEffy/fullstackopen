c6763943761193a33a782139364d77ed77420388
Stan_Effy <47476119+StanEffy@users.noreply.github.com>
2022-09-06 18:12:05 +0300

    1.4

--- part1/1.1/src/Components/Content/Content.js
+++ part1/1.1/src/Components/Content/Content.js
@@ -1,12 +1,12 @@
 import React from "react"
 import Part from "../Part/Part"
 
-const Content = ({ part1, exc1, part2, exc2, part3, exc3 }) => {
+const Content = ({ parts }) => {
 	return (
 		<>
-			<Part part={part1} exc={exc1} />
-			<Part part={part2} exc={exc2} />
-			<Part part={part3} exc={exc3} />
+			<Part part={parts[0].name} exc={parts[0].exercises} />
+			<Part part={parts[1].name} exc={parts[1].exercises} />
+			<Part part={parts[2].name} exc={parts[2].exercises} />
 		</>
 	)
 }
