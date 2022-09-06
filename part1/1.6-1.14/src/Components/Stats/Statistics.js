bdce363005f0f52fe31640cc093cbe1152531683
Stan_Effy <47476119+StanEffy@users.noreply.github.com>
2022-09-06 18:59:33 +0300

    1.9-1.10

--- part1/1.6-1.14/src/Components/Stats/Statistics.js
+++ part1/1.6-1.14/src/Components/Stats/Statistics.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import StatisticLine from "./StatisticLine";
 
 const Statistics = ({all, good, neutral, bad}) => {
     return (
         <div>
             <h2>statistics</h2>
             {all > 0 ? <>
-            <p>good {good}</p>
-            <p>neutral {neutral}</p>
-            <p>bad {bad}</p>
+                <StatisticLine text={"good"} value={good}/>
+                <StatisticLine text={"neutral"} value={neutral}/>
+                <StatisticLine text={"bad"} value={bad}/>
            </> : <p>No feedback given</p>}
         </div>
     );
