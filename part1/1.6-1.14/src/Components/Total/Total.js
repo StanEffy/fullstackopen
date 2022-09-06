e87314447b93917d25430346f9a50ca530b57899
Stan_Effy <47476119+StanEffy@users.noreply.github.com>
2022-09-06 19:07:32 +0300

    some fixes

--- part1/1.6-1.14/src/Components/Total/Total.js
+++ part1/1.6-1.14/src/Components/Total/Total.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Total = ({all, average, positive}) => {
     return (
         <table>
-            {all > 0 ?<tr> <th>All</th> <td>{all}</td></tr>:null}
-            {all > 0 ? <tr> <th>Average</th> <td>{average}</td></tr> : null}
-            {all > 0 ? <tr> <th>Positive</th> <td>{positive * 100}%</td></tr> : null}
+            {all > 0 ? <tr><th>All</th><td>{all}</td></tr>:null}
+            {all > 0 ? <tr><th>Average</th><td>{average}</td></tr> : null}
+            {all > 0 ? <tr><th>Positive</th><td>{positive * 100}%</td></tr> : null}
         </table>
     );
 };
