9708451e71b12c34ea8b4566841696b5f0e10448
Stan_Effy <47476119+StanEffy@users.noreply.github.com>
2022-09-06 18:52:18 +0300

    1.7

--- part1/1.6-1.14/src/App.js
+++ part1/1.6-1.14/src/App.js
@@ -1,15 +1,21 @@
 import Feedback from "./Components/Feedback/Feedback";
 import Statistics from "./Components/Stats/Statistics";
 import {useState} from "react";
+import Total from "./Components/Total/Total";
 
 function App() {
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
+
+    const countAll = () => good + neutral + bad
+    const countAverage = () => (good - bad)  / countAll()
+    const countPositive = () => good / countAll()
   return (
     <div className="App">
       <Feedback setGood={setGood} setNeutral={setNeutral} setBad={setBad}/>
       <Statistics good={good} bad={bad} neutral={neutral}/>
+      <Total all={countAll()} average={countAverage()} positive={countPositive()}/>
     </div>
   );
 }
--- part1/1.6-1.14/src/Components/Total/Total.js
+++ part1/1.6-1.14/src/Components/Total/Total.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-const Total = () => {
+const Total = ({all, average, positive}) => {
     return (
         <div>
-            
+          <p>All {all}</p>
+            {all > 0 ? <p>average {average}</p> : null}
+            {all > 0 ? <p>positive {positive * 100}%</p> : null}
         </div>
     );
 };
