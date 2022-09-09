89259b7b04afda63c13b91ef694b824ea6973d6e
Stan_Effy <47476119+StanEffy@users.noreply.github.com>
2022-09-09 20:47:01 +0300

    finish all -2.18

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,29 +1,78 @@
-import {useRef, useState} from 'react'
+import { useEffect, useRef, useState } from "react"
+import Form from "./Components/Form/Form"
+
+import { addNumber, deleteNumber, getNumbers, updateNumber } from "./api/api"
 
 const App = () => {
-	const [persons, setPersons] = useState([
-		{ name: 'Arto Hellas' }
-	])
-	const [newName, setNewName] = useState('')
-	const ref = useRef()
+	const [persons, setPersons] = useState([])
+	useEffect(() => {
+		getNumbers().then((data) => {
+			setPersons([...data])
+		})
+	}, [])
+
+	useEffect(() => {}, [persons])
+	const filter = useRef()
+	const name = useRef()
+	const number = useRef()
+
+	const handleDelete = (id) => {
+		deleteNumber(id).then(() =>
+			setPersons((prev) => [...prev.filter((p) => p.id !== id)])
+		)
+	}
+
+	const confirmNewNum = (newPerson, persons) => {
+		const { id } = persons.find((p) => p.name === newPerson.name)
+		confirm(
+			`${newPerson.name} is already added to phonebook, replace the old number`
+		)
+			? updateNumber(id, newPerson).then((res) =>
+					setPersons((prev) => [
+						...prev.filter((n) => n.id !== id),
+						newPerson,
+					])
+			  )
+			: null
+	}
+
 	const handleSubmit = (e) => {
 		e.preventDefault()
-		!persons.find(p => p.name === ref.current.value.trim()) ? setPersons((prev) => ([...prev, {name: ref.current.value}])) : alert(`${ref.current.value} is already added to phonebook`)
+		const newPerson = {
+			name: name.current.value.trim(),
+			number: number.current.value,
+		}
+
+		!persons.find((p) => p.name === newPerson.name)
+			? addNumber(newPerson).then((response) => {
+					response.status === 201
+						? setPersons((prev) => [...prev, response.data])
+						: alert("Something went wrong")
+			  })
+			: confirmNewNum(newPerson, persons)
 	}
 
 	return (
 		<div>
 			<h2>Phonebook</h2>
-			<form>
-				<div>
-					name: <input ref={ref}/>
-				</div>
-				<div>
-					<button type="submit" onClick={(e) => handleSubmit(e)}>add</button>
-				</div>
-			</form>
+			<div>
+				<span>Filter:</span>
+				<input type="text" ref={filter} />
+			</div>
+			<Form name={name} number={number} handleSubmit={handleSubmit} />
 			<h2>Numbers</h2>
-			{persons.map(p => <p key={p.name}>{p.name}</p>)}
+			<ul>
+				{persons.map((p) => (
+					<li>
+						<p key={p.name}>
+							{p.name} {p.number}
+						</p>
+						<button onClick={() => handleDelete(p.id)}>
+							delete
+						</button>
+					</li>
+				))}
+			</ul>
 		</div>
 	)
 }
---
+++ part2/phonebook/src/api/api.js
@@ -0,0 +1,14 @@
+import axios from "axios"
+
+export const api = axios.create({ baseURL: "http://localhost:3005/numbers" })
+
+export const getNumbers = () => api.get("/").then((res) => res.data)
+
+export const updateNumber = (id, newPerson) => api.put(`/${id}`, newPerson)
+
+export const addNumber = (newPerson) =>
+	api.post("/", newPerson).then((response) => {
+		return response.data
+	})
+
+export const deleteNumber = (id) => api.delete(`/${id}`).then((res) => id)
