a319adfe1fc343a9b1c8e2c3d902bc2590c8b694
Stan_Effy <47476119+StanEffy@users.noreply.github.com>
2022-09-10 22:05:58 +0300

    3.2-3.6

--- part3/node1/index.js
+++ part3/node1/index.js
@@ -24,9 +24,13 @@ let phonebook = [
 	}
 ]
 
+app.use(express.json())
+
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
+
+
 
 app.get('/api/persons', (request, response) => {
   response.json(phonebook)
@@ -48,6 +52,41 @@ app.get('/api/persons/:id', (request, response) => {
   } else {
     response.status(404).end()
   }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+	const id = req.params.id
+	phonebook = phonebook.filter(p => p.id != id)
+
+  response.status(204).end()
+})
+
+app.post('/api/persons', (req, res) => {
+	const body = req.body
+
+	console.log(req)
+	if(phonebook.find(p => p.name === body.name.trim())){
+		res.status(400).json({
+			error: 'name should be unique'
+		})
+	}
+
+	if (!body.name || !body.number) {
+    return res.status(400).json({ 
+      error: 'number or name is missing' 
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+
+    id: Math.floor(Math.random() * 5959),
+  }
+
+  phonebook = phonebook.concat(person)
+
+  res.json(person)
 })
 
 const PORT = 3001
