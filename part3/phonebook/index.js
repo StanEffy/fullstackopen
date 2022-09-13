bad91f518c4921167aac00993b72de54471d6af8
Stan_Effy <47476119+StanEffy@users.noreply.github.com>
2022-09-13 16:15:47 +0300

    add error handling properly all done

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,114 +1,107 @@
-const express = require('express')
-const app = express()
-const morgan = require('morgan')
-const cors = require('cors')
-const mongoose = require('mongoose')
-const Phone = require('./schema/phone')
-const errorHandler = require('./error-handler')
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const Phone = require("./schema/phone");
+const errorHandler = require("./error-handler");
 
+app.use(cors());
+app.use(express.json());
 
+morgan.token("body", (req) => {
+  return JSON.stringify(req.body);
+});
 
-app.use(cors())
-app.use(express.json())
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
 
-morgan.token('body', req => {
-  return JSON.stringify(req.body)
-})
+app.get("/", (request, res) => {
+  res.send("<h1>Hello World!</h1>");
+});
 
-app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"))
+app.get("/api/persons", (request, res) => {
+  Phone.find({}).then((result) => {
+    res.json(result);
+  });
+});
 
-
-app.get('/', (request, res) => {
-  res.send('<h1>Hello World!</h1>')
-})
-
-
-app.get('/api/persons', (request, res) => {
-	Phone.find({}).then(result => {
-		res.json(result)
-	})
-})
-
-
-
-app.get('/info', (request, res) => {
+app.get("/info", (request, res) => {
   res.send(`
 	<div>
 		<p>Phonebook has info for ${phonebook.length} people</p>
 		<p>${new Date()}</p>
-	</div>`)
-})
+	</div>`);
+});
 
-app.get('/api/persons/:id', (req, res, next) => {
-  const id = req.params.id
-	Phone.findById(id).then(phone => {
-		phone ? res.json(phone) : res.status(404).end()
-	}).catch(e => next(e))
-})
+app.get("/api/persons/:id", (req, res, next) => {
+  const id = req.params.id;
+  Phone.findById(id)
+    .then((phone) => {
+      phone ? res.json(phone) : res.status(404).end();
+    })
+    .catch((e) => next(e));
+});
 
-app.delete('/api/persons/:id', (req, res) => {
-	const id = req.params.id
+app.delete("/api/persons/:id", (req, res, next) => {
+  const id = req.params.id;
 
-	Phone.findByIdAndRemove(id).then(phone => {
-    res.status(204).end()
-  }).catch(error => next(error))
-})
+  Phone.findByIdAndRemove(id)
+    .then(() => {
+      res.status(204).end();
+    })
+    .catch((error) => next(error));
+});
 
-app.post('/api/persons', (req, res) => {
-	const body = req.body
-  console.log(body)
-	Phone.findOne({name : body.name.trim()}).then(
-		phone => {
-			if(phone){
-				return res.status(400).json({
-			error: 'name should be unique'
-		})
-			} else if(!body.name || !body.number) {
-				return res.status(400).json({ 
-					error: 'number or name is missing' 
-				})
-			}
-		
-		const person = new Phone({
-    name: body.name,
-    number: body.number,
+app.post("/api/persons", (req, res, next) => {
+  const body = req.body;
 
-  })
-		console.log(person)
-	person.save().then(newPerson => {
-    res.json(newPerson)
-  })
+  const { name, number } = body;
 
-  res.json(person)
-		} 
-		
-		
-		
-	)
-	
+  Phone.findOne({ name: name.trim() }).then((phone) => {
+    if (phone) {
+      return res.status(400).json({
+        error: "name should be unique",
+      });
+    } else if (!body.name || !body.number) {
+      return res.status(400).json({
+        error: "number or name is missing",
+      });
+    }
 
-  
-})
-app.put('/api/persons/:id', (req, res, next) => {
-  const body = req.body
+    const person = new Phone({
+      name: name,
+      number: number,
+    });
+    person
+      .save()
+      .then((newPerson) => {
+        res.json(newPerson);
+      })
+      .catch((e) => next(e));
+  });
+});
+app.put("/api/persons/:id", (req, res, next) => {
+  const body = req.body;
 
   const num = {
     name: body.name,
     number: body.number,
-		id: body.id,
-  }
+    id: body.id,
+  };
 
   Phone.findByIdAndUpdate(req.params.id, num, { new: true })
-    .then(updatedNumber => {
-      res.json(updatedNumber)
+    .then((updatedNumber) => {
+      res.json(updatedNumber);
     })
-    .catch(error => next(error))
-})
+    .catch((error) => next(error));
+});
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
+  response.status(404).send({ error: "unknown endpoint" });
+};
 
-app.use(unknownEndpoint)
-app.use(errorHandler)
+app.use(unknownEndpoint);
+app.use(errorHandler);
 
 module.exports = app;
--- part3/phonebook/schema/phone.js
+++ part3/phonebook/schema/phone.js
@@ -1,10 +1,16 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const phoneSchema = new mongoose.Schema({
-	name: {type: String, unique: true, require: true, trim: true},
-	number: {type: String, require: true},
-})
+  name: {
+    type: String,
+    unique: true,
+    require: true,
+    trim: true,
+    minLength: [3, "Minimum length is three letters"],
+  },
+  number: { type: String, require: true },
+});
 
-const Phone = mongoose.model("Phone", phoneSchema, "phonebook")
+const Phone = mongoose.model("Phone", phoneSchema, "phonebook");
 
-module.exports = Phone
+module.exports = Phone;
