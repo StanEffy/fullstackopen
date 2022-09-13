b67ff7b3a4edb60ec067bacdb585adf674574077
Stan_Effy <47476119+StanEffy@users.noreply.github.com>
2022-09-13 16:41:15 +0300

    add linter

--- part3/phonebook/schema/phone.js
+++ part3/phonebook/schema/phone.js
@@ -1,16 +1,25 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
 
 const phoneSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    unique: true,
-    require: true,
-    trim: true,
-    minLength: [3, "Minimum length is three letters"],
-  },
-  number: { type: String, require: true },
-});
+    name: {
+        type: String,
+        unique: true,
+        required: [true, "User name is required"],
+        trim: true,
+        minLength: [3, "Minimum length of the name is three letters"],
+    },
+    number: {
+        type: String,
+        required: [true, "User phone number required"],
+        validate: {
+            validator: (v) => {
+                return /\d{2,3}-\d{6,}/.test(v)
+            },
+            message: (props) => `${props.value} is not a valid phone number!`,
+        },
+    },
+})
 
-const Phone = mongoose.model("Phone", phoneSchema, "phonebook");
+const Phone = mongoose.model("Phone", phoneSchema, "phonebook")
 
-module.exports = Phone;
+module.exports = Phone
--- part3/phonebook/server.js
+++ part3/phonebook/server.js
@@ -1,14 +1,14 @@
-const dotenv = require("dotenv");
-const mongoose = require("mongoose");
-const app = require("./index.js");
+const dotenv = require("dotenv")
+const mongoose = require("mongoose")
+const app = require("./index.js")
 
-dotenv.config({ path: "./config.env" });
+dotenv.config({ path: "./config.env" })
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001
 
-const DB = process.env.DATABASE.replace("password", process.env.PASSWORD);
-mongoose.connect(DB).then(() => console.log("DB connection successful!"));
+const DB = process.env.DATABASE.replace("password", process.env.PASSWORD)
+mongoose.connect(DB).then(() => console.log("DB connection successful!"))
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+    console.log(`Server running on port ${PORT}`)
+})
