96796861a1089ab0d5b00324c5396cafadf99db5
Stan_Effy <47476119+StanEffy@users.noreply.github.com>
2022-09-25 18:24:06 +0300

    -7.8

--- part7/country-hook/src/App.js
+++ part7/country-hook/src/App.js
@@ -18,7 +18,12 @@ const useField = (type) => {
 const useCountry = (name) => {
   const [country, setCountry] = useState(null)
 
-  useEffect(() => {})
+  useEffect(() => {
+    axios.get(`https://restcountries.com/v3.1/name/${name}?fullText=true`).then(res => setCountry(res.data[0])).catch(e => {
+      setCountry(null)
+    })
+    console.log(country, name)
+  }, [name, country])
 
   return country
 }
@@ -31,11 +36,11 @@ const Country = ({ country }) => {
   return (
     <div>
       <h3>{country.name.common}</h3>
-      <div>population {country.population}</div> 
+      <div>population {country.population}</div>
       <div>capital {country.capital}</div>
-      <img src={country.flags.png} height='100' alt={`flag of ${country.name.common}`}/> 
+      <img src={country.flags.png} height='100' alt={`flag of ${country.name.common}`}/>
     </div>
-  )  
+  )
 }
 
 const App = () => {
