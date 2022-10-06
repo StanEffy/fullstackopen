f279828d293c3cf9f7ee173f763188168c52f148
Stan_Effy <47476119+StanEffy@users.noreply.github.com>
2022-10-06 13:58:57 +0300

    create types for Patient and Diagnose

---
+++ part9/patientor-back/types/types.ts
@@ -0,0 +1,13 @@
+export type Diagnose = {
+    code: String,
+    name: String,
+    latin?: String
+}
+export type Patient = {
+    id: String,
+    name: String,
+    dateOfBirth: String,
+    ssn: String,
+    gender: String,
+    occupation: String
+}
