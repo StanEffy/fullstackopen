e2430149690fa638da3232fb88275a0e62da75af
Stan_Effy <47476119+StanEffy@users.noreply.github.com>
2022-10-06 21:06:50 +0300

    9.13

--- part9/patientor-back/routes/patients.ts
+++ part9/patientor-back/routes/patients.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import patientsService from "../services/patients";
 import {Patient} from "../types/types";
+import {isString} from "../utils/utils";
 
 interface TypedRequestBody<T> extends Express.Request {
     body: T
@@ -14,6 +15,10 @@ router.get('/', (_req, res) => {
 });
 
 router.post('/', (req:TypedRequestBody<Omit<Patient, "id">>, res) => {
+    const {body} = req;
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    Object.keys(body).map(k => isString(body[k]));
     const addPatient = patientsService.addPatient(req.body);
     res.send(addPatient);
 });
