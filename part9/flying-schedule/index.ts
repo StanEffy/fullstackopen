abe61efba81102b2c08e50bde429fd62075fb423
Stan_Effy <47476119+StanEffy@users.noreply.github.com>
2022-10-06 21:25:56 +0300

    add index file

--- part9/flying-schedule/index.ts
+++ part9/flying-schedule/index.ts
@@ -1,7 +1,23 @@
 import express from 'express';
 const app = express();
 app.use(express.json());
-
+const table = [
+    {
+        "id": 1,
+        "date": "2017-01-01",
+        "weather": "rainy",
+        "visibility": "poor",
+        "comment": "Pretty scary flight, I'm glad I'm alive"
+    },
+    {
+        "id": 2,
+        "date": "2017-04-01",
+        "weather": "sunny",
+        "visibility": "good",
+        "comment": "Everything went better than expected, I'm learning much"
+    },
+]
+console.log(table)
 const PORT = 3000;
 
 app.get('/ping', (_req, res) => {
