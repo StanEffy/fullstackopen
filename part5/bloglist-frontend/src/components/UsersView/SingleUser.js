4095b50925da28bb5cc9c50e7a61a74789bff4cb
Stan_Effy <47476119+StanEffy@users.noreply.github.com>
2022-10-14 15:55:00 +0300

    style single user view

--- part5/bloglist-frontend/src/components/UsersView/SingleUser.js
+++ part5/bloglist-frontend/src/components/UsersView/SingleUser.js
@@ -1,6 +1,8 @@
 import React from "react"
 import { useParams, useNavigate } from "react-router-dom"
 import { useSelector } from "react-redux"
+import { Button, Card, CardContent, Typography } from "@mui/material"
+// import PropTypes from "prop-types"
 
 const SingleUser = () => {
 	const id = useParams().id
@@ -9,19 +11,41 @@ const SingleUser = () => {
 	const allPosts = useSelector((state) =>
 		state.blogs.filter((b) => b.user.id === id)
 	)
-	console.log(allPosts)
+
 	return (
-		<div>
-			{allPosts.map((s) => (
-				<p key={"story_" + s.id}>
-					<span>Author: {s.author} </span>
-					<span>Title: {s.title} </span>
-					<span>Likes: {s.likes} </span>
-				</p>
-			))}
-			<button onClick={() => navigate("/")}>back to all</button>
-		</div>
+		<Card sx={{ maxWidth: "500px", margin: "20px auto" }}>
+			<CardContent>
+				<Typography variant={"h6"} textAlign={"center"}>
+					List of blogs by
+				</Typography>
+				{allPosts.map((s) => (
+					<Typography
+						variant={"body2"}
+						key={"story_" + s.id}
+						sx={{
+							border: "1px solid blue",
+							borderRadius: "10px",
+							mb: 1,
+							p: 0.5,
+						}}
+					>
+						<Typography>Title: {s.title} </Typography>
+						<Typography>Likes: {s.likes} </Typography>
+					</Typography>
+				))}
+			</CardContent>
+
+			<Button
+				variant={"contained"}
+				onClick={() => navigate("/")}
+				sx={{ margin: "20px auto", display: "block" }}
+			>
+				back to all
+			</Button>
+		</Card>
 	)
 }
-
+// SingleUser.propTypes = {
+// 	author: PropTypes.string,
+// }
 export default SingleUser
