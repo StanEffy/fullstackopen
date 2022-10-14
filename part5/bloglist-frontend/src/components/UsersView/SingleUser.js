7d68cb93fba38d6d68c68b58113f0d4ffe63bd4d
Stan_Effy <47476119+StanEffy@users.noreply.github.com>
2022-10-14 15:58:53 +0300

    fix link in single user component

--- part5/bloglist-frontend/src/components/UsersView/SingleUser.js
+++ part5/bloglist-frontend/src/components/UsersView/SingleUser.js
@@ -37,7 +37,7 @@ const SingleUser = () => {
 
 			<Button
 				variant={"contained"}
-				onClick={() => navigate("/")}
+				onClick={() => navigate("/users")}
 				sx={{ margin: "20px auto", display: "block" }}
 			>
 				back to all
