91a6334364bdd82759614433136cab23b48936c0
Stan_Effy <47476119+StanEffy@users.noreply.github.com>
2022-10-14 15:19:16 +0300

    style single blog page

--- part5/bloglist-frontend/src/components/SingleBlogPost/SingleBlogPost.js
+++ part5/bloglist-frontend/src/components/SingleBlogPost/SingleBlogPost.js
@@ -4,6 +4,15 @@ import { useParams } from "react-router-dom"
 import { voteBlog, addCommentToBlog } from "../../store/blogsReducer"
 import { setNotify } from "../../store/notificationReducer"
 import Button from "@mui/material/Button"
+import {
+	Box,
+	Card,
+	CardContent,
+	ListItem,
+	TextField,
+	Typography,
+	List,
+} from "@mui/material"
 const SingleBlogPost = () => {
 	const { id } = useParams()
 	const dispatch = useDispatch()
@@ -32,26 +41,47 @@ const SingleBlogPost = () => {
 
 	if (!blog) return <p>There is no such blog</p>
 	return (
-		<div>
-			<h4>Title: {blog.title}</h4>
-			<a href={blog.url}>Link to a blog</a>
-			<div>
-				<p>{blog.likes} liked it!</p>
-				<button onClick={() => handleLike(blog)}>Ay! Me gusto!</button>
-			</div>
-			<h5>Author: {blog.author}</h5>
-			<input type={"text"} name={"comment"} ref={comment} />
-			<Button variant="contained" onClick={() => handleAddComment()}>
-				add comment
-			</Button>
-			{blog.comments ? (
-				<ul>
-					{blog.comments.map((c, i) => (
-						<li key={id + "-comments-" + i}>{c}</li>
-					))}
-				</ul>
-			) : null}
-		</div>
+		<Card sx={{ maxWidth: "500px", m: "20px auto" }}>
+			<CardContent>
+				<Typography variant={"h5"}>Title: {blog.title}</Typography>
+				<a href={blog.url}>Link to a blog</a>
+				<Box>
+					<Typography variant="body2">
+						{blog.likes} liked it!
+					</Typography>
+					<Button
+						variant="contained"
+						onClick={() => handleLike(blog)}
+					>
+						Ay! Me gusto!
+					</Button>
+				</Box>
+				<Typography variant={"h6"}>Author: {blog.author}</Typography>
+				<Box display={"flex"} flexDirection={"column"}>
+					<TextField
+						type={"text"}
+						name={"comment"}
+						ref={comment}
+						label={"Your comment"}
+					/>
+					<Button
+						variant="contained"
+						sx={{ my: 1 }}
+						onClick={() => handleAddComment()}
+					>
+						add comment
+					</Button>
+				</Box>
+
+				{blog.comments ? (
+					<List>
+						{blog.comments.map((c, i) => (
+							<ListItem key={id + "-comments-" + i}>{c}</ListItem>
+						))}
+					</List>
+				) : null}
+			</CardContent>
+		</Card>
 	)
 }
 
